e778642c965d451ab61c4838aa1d8f128c3ee715
bryanbill <emobrya@gmail.com>
2024-01-23 11:25:25 +0300

    ft: adds multpiple relations for idb

--- src/db.js
+++ src/db.js
@@ -1,5 +1,10 @@
 import { openDB } from 'idb';
 
+const Relations = {
+    coordinates: "coordinates",
+    accuracy: "accuracy"
+};
+
 /**
  * Opens or creates a database.
  * 
@@ -9,10 +14,14 @@ const db = async () => {
     try {
         return await openDB('locator', 1, {
             upgrade(db) {
-                if (!db.objectStoreNames.contains('coordinates')) {
-                    db.createObjectStore('coordinates', {
-                        autoIncrement: true,
-                    });
+                const relations = Object.values(Relations);
+                console.log(relations);
+                for (const relation of relations) {
+                    if (!db.objectStoreNames.contains(relation)) {
+                        db.createObjectStore(relation, {
+                            autoIncrement: true,
+                        });
+                    }
                 }
             },
         });
@@ -25,15 +34,18 @@ const db = async () => {
 /**
  * 
  * @param {import('ol/coordinate').Coordinate} coords 
+ * @param {Object?} props - Key-value pairs to store with the coordinates.
+ * @param {string?} table - The table to store the data in, defaults to "coordinates".
  */
-const save = (coords) => {
+const save = (coords, props = null, table = "coordinates") => {
     if (!coords) return;
 
     db().then((db) => {
-        const tx = db.transaction('coordinates', 'readwrite');
+        const tx = db.transaction(table, 'readwrite');
         tx.store.add({
             coords,
             timestamp: Date.now(),
+            ...(props ? props : {}),
         });
         return tx.done;
     }).catch(console.error);
@@ -76,4 +88,5 @@ const get = async (start, end, limit = 100, offset = 0) => {
 export {
     save,
     get,
+    Relations,
 };
