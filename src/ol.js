34f63ed86f96aa4ef892d7b5e08c8fea60e1e3b1
bryanbill <emobrya@gmail.com>
2024-01-24 16:35:20 +0300

    adds draw path fn

--- src/ol.js
+++ src/ol.js
@@ -6,6 +6,7 @@ import { Style, Stroke, Icon } from "ol/style";
 
 import { save, get, Relations } from "./db";
 import man from "../assets/icons/man.png";
+import { dialog } from "./ui";
 
 /**
  * @type {Map}
@@ -79,7 +80,7 @@ const init = () => {
 
     get(Date.now() - 1000 * 60 * 60 * 24, Date.now()).then((result) => {
         if (!result || !result.length) return;
-        
+
         coords.push(...result);
         drawPath();
     });
@@ -120,10 +121,23 @@ const locate = () => {
     })
 
     geolocation.on("error", console.error);
+
 };
 
 
 const drawPath = () => {
+    if (!coords || !coords.length) {
+        pathFeature.setGeometry(null);
+        view.animate({
+            center: [0, 0],
+            zoom: 2,
+            duration: 1000,
+        });
+        
+        dialog("Error", "No coordinates to draw path.");
+        return;
+    }
+
     pathFeature.setGeometry(new LineString(coords));
     pathFeature.setStyle(new Style({
         stroke: new Stroke({
@@ -151,31 +165,37 @@ const filterTool = () => {
     startInput.addEventListener("change", (e) => {
         start = new Date(e.target.value).getTime();
 
+        if (!end) return;
 
-        if (end) {
-            if (start > end) {
-                alert("Start date must be before end date");
-                return;
-            }
-            get(start, end).then((result) => {
-                coords = result;
-
-                console.log(coords);
-                drawPath();
-            });
+        if (start > end) {
+            dialog("Error", "Start date must be before end date.");
+            return;
         }
+
+        get(start, end).then((result) => {
+            coords = result;
+
+            console.log(coords);
+            drawPath();
+        });
     })
 
     endInput.addEventListener("change", (e) => {
         end = new Date(e.target.value).getTime();
 
-        if (start && (start < end)) {
-            get(start, end).then((result) => {
-                coords = result;
-                console.log(coords);
-                drawPath();
-            });
+        if (!start) return;
+
+        if (end < start) {
+            dialog("Error", "Start date must be before end date.");
+            return;
         }
+
+        get(start, end).then((result) => {
+            coords = result;
+            console.log(coords);
+            drawPath();
+        });
+
     })
 }
 
